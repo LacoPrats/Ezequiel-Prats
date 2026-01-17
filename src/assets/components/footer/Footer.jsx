0d28d95a7290ee822385e919a79a37232b68dc2f
LacoPrats <eze.prats13@gmail.com>
2026-01-17 19:55:31 -0300

    Fix footer anchor links

--- src/assets/components/footer/Footer.jsx
+++ src/assets/components/footer/Footer.jsx
@@ -1,25 +1,30 @@
-import React from 'react'
-import './footer.css'
-
+import React from "react";
+import "./footer.css";
 
 const Footer = () => {
   return (
     <footer>
-      <a href="#"className='footer__logo'>Ezequiel Prats</a>
-      <ul className='permalinks'>
-        <li><a href="#">Home</a></li>
+      {/* Logo: usar link real a una sección */}
+      <a href="#header" className="footer__logo">
+        Ezequiel Prats
+      </a>
+
+      <ul className="permalinks">
+        {/* Home: apuntar a una sección válida */}
+        <li><a href="#header">Home</a></li>
         <li><a href="#experience">Skills</a></li>
         <li><a href="#academic">Education</a></li>
         <li><a href="#portfolio">Portfolio</a></li>
         <li><a href="#contact">Contact</a></li>
       </ul>
-      <div className="footer__socials">
-      </div>
+
+      <div className="footer__socials" />
+
       <div className="footer__copyright">
-        <small>copy; Ezequiel Prats</small>
+        <small>&copy; {new Date().getFullYear()} Ezequiel Prats</small>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
