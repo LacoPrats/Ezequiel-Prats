8a9c67fa000b87c47e7fdf1a6b13911aff8fe10f
LacoPrats <eze.prats13@gmail.com>
2026-01-17 19:27:38 -0300

    cambios

--- src/assets/components/footer/Footer.jsx
+++ src/assets/components/footer/Footer.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import './footer.css'
-import  {BsLinkedin}  from 'react-icons/bs'
-import {FaGithub} from 'react-icons/fa'
-import {FaBehanceSquare} from 'react-icons/fa'
+
 
 const Footer = () => {
   return (
@@ -16,8 +14,6 @@ const Footer = () => {
         <li><a href="#contact">Contact</a></li>
       </ul>
       <div className="footer__socials">
-      <a href="https://www.linkedin.com/in/ezequiel-prats-4369311b1/" target="_blank"><BsLinkedin/></a>
-<a href="http://github.com/LacoPrats" target="_blank"><FaGithub/></a>
       </div>
       <div className="footer__copyright">
         <small>copy; Ezequiel Prats</small>
--- src/assets/components/header/Headersocials.jsx
+++ src/assets/components/header/Headersocials.jsx
@@ -5,8 +5,8 @@ import {FaGithub} from 'react-icons/fa'
 const Headersocials = () => {
   return (
     <div className='header_socials'>
-<a href="https://www.linkedin.com/in/ezequiel-prats-4369311b1/" target="_blank"><BsLinkedin/></a>
-<a href="https://www.github.com" target="_blank"><FaGithub/></a>
+<a href="https://www.linkedin.com/in/ezequiel-prats-4369311b1/" target="_blank" rel="noreferrer"><BsLinkedin/></a>
+<a href="https://www.github.com" target="_blank" rel="noreferrer"><FaGithub/></a>
     </div>
   )
 }
